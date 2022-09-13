131842ee9a586069e4c7827340f73e638c4a304c
tomas-cunha <tcunha930@gmail.com>
2022-09-13 11:21:50 -0300

    exericio01

--- devinknowledge/src/components/CardList.jsx
+++
@@ -1,8 +0,0 @@
-import PropTypes from "prop-types";
-export const CardList = ({ children }) => {
-  return <div className="cardList">{children}</div>;
-};
-
-CardList.propTypes = {
-  children: PropTypes.node,
-};
--- devinknowledge/src/main.jsx
+++ devinknowledge/src/main.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./App";
 
-import "./assets/css/styles.css";
+const App = () => {
+  return "oi";
+};
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
